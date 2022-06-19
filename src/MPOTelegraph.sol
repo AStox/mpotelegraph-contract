4725237cbff0348d7c2f470c4ae1f8517d4ba1c0
Adam Stockermans <adamstox@protonmail.com>
2022-06-19 13:09:42 -0500

    Add reply functionality

--- src/MPOTelegraph.sol
+++ src/MPOTelegraph.sol
@@ -5,14 +5,6 @@ pragma solidity ^0.8.13;
 
 /// @dev Note that this is a very stripped down ERC-721 knock-off with gas savings as the highest priority. Some features may be unsafe or missing.
 
-struct Telegram {
-    address from;
-    bytes32 line1;
-    bytes32 line2;
-    bytes32 line3;
-    bytes32 line4;
-}
-
 contract MPOTelegraph {
     address private owner;
     uint256 public PRICE = 1000000000000000; // 0.001 eth
@@ -23,9 +15,14 @@ contract MPOTelegraph {
         owner = msg.sender;
     }
 
+    struct Telegraph {
+        address to;
+        address from;
+    }
+
     // ERC721 --------------------------------------------------------------->>
     mapping(uint256 => address) private ownership;
-    mapping(uint256 => Telegram) private metadata;
+    mapping(uint256 => Telegraph) private toFrom;
     mapping(uint256 => address) private approvedForToken;
     mapping(address => mapping(address => bool)) private approvedForAll;
     event Transfer(address indexed from, address indexed to, uint256 indexed tokenId);
@@ -100,22 +97,24 @@ contract MPOTelegraph {
         require(msg.value >= PRICE, "Send more ETH");
         require(ownership[id] == address(0), "ID already in use");
 
-        ownership[id] = to;
-        // metadata[id] = Telegram(msg.sender, s2b32(line1),s2b32(line2),s2b32(line3),s2b32(line4));
-        // metadata[id] = input;
+        ownership[id] = msg.sender;
+        toFrom[id] = Telegraph(to, msg.sender);
 
         emit Transfer(address(0), to, id);
         emit Mint(msg.sender, id, text);
     }
 
-    // function reply(uint256 burnId, string calldata line1, string calldata line2, string calldata line3, string calldata line4) public {
-    //     require(msg.sender == ownership[burnId] || msg.sender == getApproved(burnId) || isApprovedForAll(ownership[burnId], msg.sender), "Unauthorized");
-
-    //     emit Transfer(ownership[burnId], address(0), burnId);
-    //     ownership[burnId] = metadata[burnId].from;
-    //     emit Transfer(address(0), metadata[burnId].from, burnId);
-    //     metadata[burnId] = Telegram(msg.sender, s2b32(line1),s2b32(line2),s2b32(line3),s2b32(line4));
-    // }
+    function reply(uint256 id, string calldata text) public {
+        require(msg.sender == ownership[id] || msg.sender == getApproved(id) || isApprovedForAll(ownership[id], msg.sender), "Unauthorized");
+        toFrom[id] = Telegraph(toFrom[id].from, toFrom[id].to);
+        ownership[id] = toFrom[id].to;
+        // Burn for housekeeping
+        emit Transfer(ownership[id], address(0), id);
+        // Transfer to prev 'from' address (now 'to' address) to bypass marketplace hidden folder rules
+        emit Transfer(address(0), ownership[id], id);
+        // Mint for the new message
+        emit Mint(msg.sender, id, text);
+    }
 
     // Required by etherscan.io
     function totalSupply() public view virtual returns (uint256) {
--- test/MPOTelegraph.t.sol
+++ test/MPOTelegraph.t.sol
@@ -7,6 +7,8 @@ import "../src/MPOTelegraph.sol";
 
 contract ContractTest is Test {
     MPOTelegraph public mpo;
+    event Transfer(address indexed from, address indexed to, uint256 indexed tokenId);
+    event Mint(address indexed from, uint256 indexed tokenId, string text);
 
     function setUp() public {
         mpo = new MPOTelegraph();
@@ -15,6 +17,16 @@ contract ContractTest is Test {
     function testMintSuccess(uint256 id, address to) public {
         mpo.mint{value:0.001e18}(id, to, "hello world");
         assertTrue(mpo.ownerOf(id) == address(to));
+    }
+    function testMintTransferEvent(uint256 id, address to) public {
+        vm.expectEmit(true, true, true, true);
+        emit Transfer(address(0), to, id);
+        mpo.mint{value:0.001e18}(id, to, "hello world");
+    }
+    function testMintMintEvent(uint256 id, address to) public {
+        vm.expectEmit(true, true, true, true);
+        emit Mint(address(this), id, "hello world");
+        mpo.mint{value:0.001e18}(id, to, "hello world");
     }
     function testMintHugeMessage(uint256 id, address to) public {
         mpo.mint{value:0.001e18}(id, to, "Call me Ishmael. Some years ago never mind how long precisely having little or no money in my purse, and nothing particular to interest me on shore, I thought I would sail about a little and see the watery part of the world. It is a way I have of driving off the spleen and regulating the circulation. Whenever I find myself growing grim about the mouth; whenever it is a damp, drizzly November in my soul; whenever I find myself involuntarily pausing before coffin warehouses, and bringing up the rear of every funeral I meet; and especially whenever my hypos get such an upper hand of me, that it requires a strong moral principle to prevent me from deliberately stepping into the street, and methodically knocking peoples hats off then, I account it high time to get to sea as soon as I can. This is my substitute for pistol and ball. With a philosophical flourish Cato throws himself upon his sword; I quietly take to the ship. There is nothing surprising in this. If they but knew it, almost all men in their degree, some time or other, cherish very nearly the same feelings towards the ocean with me.");
